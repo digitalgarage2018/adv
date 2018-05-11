f93eb8a040c4d56375d27270e09f3c1d6c1b827e
milonite <milonite@hotmail.it>
2018-05-11 11:35:52 +0200

    error handlers login

--- Project3_FE/src/scenes/LogInPage/LogInPage.js
+++ Project3_FE/src/scenes/LogInPage/LogInPage.js
@@ -9,13 +9,18 @@ import {logout} from "../../services/LogoutService/LogoutService";
 
 import "./LogInPage.css";
 
+const error = {
+    color: 'red',
+    textalign:'center'};
+
 export default class Login extends Component {
 
         state = {
             userName: "",
             password: "",
             isLogged: false,
-            jwt: ""
+            jwt: "",
+            message:""
         };
 
 
@@ -40,22 +45,26 @@ export default class Login extends Component {
         })
             .then( response => {
 
-
-
                 this.setState({isLogged: true, jwt: response.headers.jwt});
                 console.log("Stato dopo la LogIn: ", this.state);
                 this.props.history.push("/");
 
             })
-            .catch(function (error) {
-                console.log(error);
+            .catch( error => {
+                if (error.response.data.server === 403)
+                    {this.setState({message:"Credenziali non corrette"})}
+
+                    else if (error.response.data.server === 0){
+                    this.setState({message:"Credenziali non corrette"})}
+
             });
 
     }
 
     logoutHandler = (event) => {
-        logout();
+
     }
+
 
 
 
@@ -95,8 +104,14 @@ export default class Login extends Component {
                     >
                         Log Out
                     </Button>
+                    <h4 style={error}> {this.state.message}</h4>
+
+
                 </form>
+
+
             </div>
+
 
         );
     }
