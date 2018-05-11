81cb0720ece969e52ac86d18c1d6e51ae7a35d96
ChiaraBaglioni <chiara.baglioni.math@gmail.com>
2018-05-11 12:18:10 +0200

    products added

--- Project3_FE/src/scenes/ProductsPage/ProductsPage.js
+++ Project3_FE/src/scenes/ProductsPage/ProductsPage.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import {productsService} from '../../services/productsPageService/productsPageService';
+import { Container, Row, Col } from 'reactstrap';
 import axios from "axios/index";
 
+import accappatoio from "./images/accappatoio.jpg";
+import candele from "./images/candele.jpeg";
+import candele2 from "./images/candele2.jpg";
+import ciabatte from "./images/ciabatte.jpg";
+import cremaCalendula from "./images/crema-calendula.jpg";
+import cremaCalendula2 from "./images/crema-calendula2.jpg";
+import cremaCamomilla from "./images/crema-camomilla.jpg";
+import shiatsu from "./images/kit-massaggio-shiatsu.jpg";
+import thai from "./images/kit-massaggio-thai.jpg";
+import oliProfumati from "./images/oli-profumati.jpg";
+import olioArgan from "./images/olio-argan.jpg";
+import telo from "./images/telo.jpg";
+
+
+import {productsService} from '../../services/productsPageService/productsPageService';
+
 import Product from './components/Product';
+
 
 
 export default class ProductsPage extends Component {
 
     state = {
-        products: []
+        products: [],
+        imagePaths: []
     }
+
+
 
 
     componentDidMount(){
         axios.get('http://localhost:8091/products/')
             .then( response => {
-                    this.setState({products: response.data});
-                    console.log(this.state.products);
+                this.setState({products: response.data});
+                this.setState({imagePaths: [
+                        "accappatoio",
+                        "candele",
+                        "candele2",
+                        "ciabatte",
+                        "cremaCalendula",
+                        "cremaCalendula2",
+                        "cremaCamomilla",
+                        "shiatsu",
+                        "thai",
+                        "oliProfumati",
+                        "olioArgan",
+                        "telo"
+                    ]});
                 }
             )
             .catch(function (error) {
@@ -30,25 +63,20 @@ export default class ProductsPage extends Component {
 
     render() {
 
-        const productList = this.state.products.map( prod => {
-           console.log(prod);
-           return <Product
-               name={prod.p_name}/>
+        const productlist = this.state.products.map( (prod, index) => {
+
+           return
+            <Product name={prod.p_name} path={this.imagePaths[index]} />
+
+
         });
-
-
-
-
 
 
         return (
             <div>
-                {productList}
-
-                
+                {productlist}
             </div>
-
-
         )
+
     }
 }
--- Project3_FE/src/scenes/ProductsPage/components/Product.js
+++ Project3_FE/src/scenes/ProductsPage/components/Product.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 
 const Product = (props) => {
-    return <div> <p> {props.name} </p></div>
+    return (
+        <div>
+            <Card>
+                <CardImg top width="100%" src={props.path} alt="(descrizione del prodotto)" />
+                <CardBody>
+                    <CardTitle>{props.name}</CardTitle>
+                    <CardSubtitle>Card subtitle</CardSubtitle>
+                    <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                    <Button>Button</Button>
+                </CardBody>
+            </Card>
+        </div>
+    );
 };
 
 export default Product;
