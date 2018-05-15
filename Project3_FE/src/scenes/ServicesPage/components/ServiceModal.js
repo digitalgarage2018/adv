12bab96f0e023e3e0f29d4814b788cb888ac6a76
milonite <milonite@hotmail.it>
2018-05-15 12:27:15 +0200

    metamask

--- Project3_FE/src/scenes/ServicesPage/components/ServiceModal.js
+++ Project3_FE/src/scenes/ServicesPage/components/ServiceModal.js
@@ -40,7 +40,7 @@ const serviceModal = (props) => {
                 </p>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={handleClick}> Paga con MetaMask </Button>
+                <Button onClick={handleClick}> Paga con MetaMaskyarn st </Button>
             </Modal.Footer>
         </Modal>
     );
