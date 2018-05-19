31298a414260f04c48b6e6b73a4f6dde1f9185de
ChiaraBaglioni <chiara.baglioni.math@gmail.com>
2018-05-19 19:52:29 +0200

    managing delete error

--- Project3_FE/src/scenes/EditServicePage/components/DeleteModal.js
+++ Project3_FE/src/scenes/EditServicePage/components/DeleteModal.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 
 import {Modal, Button} from 'react-bootstrap';
-
 import { axiosinstance } from '../../../components/AxiosInstance/AxiosInstance';
 
 
+
 const deleteModal = (props) => {
+
+    let message = "";
 
     let deleteServiceHandler = (event) => {
         event.preventDefault();
@@ -18,37 +20,13 @@ const deleteModal = (props) => {
         let instance = axiosinstance();
         instance.get(url)
             .then( response => {
-
-                console.log("response della delete: ", response );
+                console.log("response della Delete: ", response );
                 props.history.push("/MieiServizi");
-
-                /*if (response.data.server === 201)
-                {  props.history.push("/MieiServizi"); }
-                else if (response.data.server === 406){
-                    this.setState({message:"Non hai i permessi per modificare un servizio. Accedi come centro benessere"});
-                }else if(response.data.server === 504) {
-                    this.setState({message:"Sessione scaduta. Per favore accedi nuovamente"});
-                }else if(response.data.server === 403) {
-                    this.setState({message:"Per favore accedi"});
-                }else {
-                    this.setState({message:"Ci scusiamo, vi è stato un errore: "+response.data.response});
-                }*/
-
-
             })
             .catch( error => {
-                console.log('error della delete', error);
-
-                /*if (error.response.status === 400){
-                    this.setState({message:"Inserire degli interi maggiori di zero in prezzo e durata"})
-                }
-                else{
-                    this.setState({message:"Ci scusiamo, vi è stato un errore: "+error.response.data.response})
-                }*/
+                console.log('Error della Delete', error);
+                props.history.push("/MieiServizi");
             });
-
-
-
 
     }
 
